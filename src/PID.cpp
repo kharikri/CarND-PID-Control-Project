bec512a6520eac944018d3e70b61bac4b3876abc
Kris Harikrishnan <kharikri@gmail.com>
2017-06-26 17:11:56 -0700

    Add files via upload

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,19 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	Kp = 0.0;
+	Ki = 0.0;
+	Kd = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+	    p_error = cte;
+        d_error = cte - prev_cte
+        i_error += cte
+        prev_cte = cte
 }
 
 double PID::TotalError() {
+	return (p_error + i_error + d_error);
 }
 
